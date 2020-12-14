301e398e3948bca35e4d9c817793af147053d7a5
Naserume <70619561+Naserume@users.noreply.github.com>
2020-12-15 02:56:47 +0900

    Update example_bot.py

--- example_bot.py
+++ example_bot.py
@@ -3,17 +3,27 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print("We have logged in as {0.user}".format(bot))
+    print("Discord bot ID is "+str(bot.user.id))
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('카지노 일'))
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.command(aliases=["Hello","hello!","Hello!","hi","Hi","hi!","Hi!"])
+async def hello(ctx):
+    
+    await ctx.channel.send("Hello!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def embed(ctx):
+    embed=discord.Embed(title="Embed", description="Embed 내용.", color=0x00aaaa)
+    embed.set_author(name="작성자의 이름")
+    embed.add_field(name="이것은 field1입니다.", value="이것은 field1 값입니다.", inline=False)
+    embed.add_field(name="이것은 field2입니다.", value="이것은 field2 값입니다.", inline=False)
+    embed.add_field(name="이것은 field3입니다.", value="이것은 field3 값입니다.", inline=False)
+    embed.add_field(name="이것은 field4입니다.", value="이것은 field4 값입니다.", inline=False)
+    embed.set_footer(text="이것은 footer의 값입니다.")
+    await ctx.channel.send(embed=embed)
 
 bot.run('my token')
