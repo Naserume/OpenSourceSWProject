6e1be653e79ec1b0bbb8d634ecdece7c8c7c4979
Naserume <70619561+Naserume@users.noreply.github.com>
2020-12-15 05:26:33 +0900

    칩 저장기능 추가.

--- example_bot.py
+++ example_bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -25,5 +26,23 @@ async def embed(ctx):
     embed.add_field(name="이것은 field4입니다.", value="이것은 field4 값입니다.", inline=False)
     embed.set_footer(text="이것은 footer의 값입니다.")
     await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def addChip(ctx,wants=100):
+    senderid=ctx.author.id
+    player="player"+str(senderid)
+    try:
+        with open(player+".txt","r") as playerchip:
+            chip=int(playerchip.read())
+            print(chip)
+        with open(player+".txt","w") as playerchip:
+            chip = chip+wants
+            print(chip)
+            playerchip.write(str(chip))
+    except FileNotFoundError:
+        with open(player+".txt","w") as playerchip:
+            playerchip.write("100")
+    with open(player+".txt","r") as playerchip:
+        await ctx.channel.send(ctx.author.name+"씨의 칩은 "+playerchip.read()+"개")
 
 bot.run('my token')
